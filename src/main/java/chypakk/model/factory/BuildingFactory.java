256c836e025a93f8e0d7cfa10049082e6b1b26db
Daniil Yakimtsev <dan.yakimtztff@yandex.ru>
2025-09-17 15:26:45 +0300

    refactor BuildingFactory

--- src/main/java/chypakk/model/factory/BuildingFactory.java
+++ src/main/java/chypakk/model/factory/BuildingFactory.java
@@ -4,14 +4,31 @@ import chypakk.model.building.Barracks;
 import chypakk.model.building.Building;
 import chypakk.model.building.Marketplace;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Supplier;
+
 public class BuildingFactory {
+    private final Map<String, Supplier<Building>> buildingCreators;
+
+    public BuildingFactory() {
+        this.buildingCreators = createBuildingMap();
+    }
+
+    private Map<String, Supplier<Building>> createBuildingMap() {
+        Map<String, Supplier<Building>> map = new HashMap<>();
+        map.put("Marketplace", Marketplace::new);
+        map.put("Barracks", Barracks::new);
+
+        return map;
+    }
 
     public Building createBuilding(String type) {
-        return switch (type) {
-            case "Marketplace" -> new Marketplace();
-            case "Barracks" -> new Barracks();
-            default -> throw new IllegalArgumentException("Неизвестный тип здания: " + type);
-        };
+        Supplier<Building> creator = buildingCreators.get(type);
+        if (creator == null) {
+            throw new IllegalArgumentException("Неизвестный тип здания: " + type);
+        }
+        return creator.get();
     }
 
 }
